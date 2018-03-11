4c74a7d62715bfa298a359dc716948603a125895
vedadr@gmail.com <vedadr@gmail.com>
2018-03-11 22:26:34 +0100

    first commit

---
+++ data_preparation.py
@@ -0,0 +1,26 @@
+from sklearn.model_selection import train_test_split
+
+
+def generate_features(dataset):
+    pass
+
+
+def fix_missing_values(dataset):
+    pass
+
+
+def fix_outliers(dataset):
+    pass
+
+
+def data_preparation(dataset):
+    generate_features(dataset)
+    fix_missing_values(dataset)
+    fix_outliers(dataset)
+    x = dataset[:,:8]
+    y = dataset[:, 8]
+    seed = 7
+    test_size = 0.33
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=seed)
+
+    return {'x_train': x_train, 'x_test': x_test, 'y_train': y_train, 'y_test': y_test}
